ec17fd58e2ab5936aa99b3bea12ddd650f2a1472
ihub <long.dizhi@gmail.com>
2026-08-21 15:08:32 +0800

    Update manage.sh

--- manage.sh
+++ manage.sh
@@ -90,7 +90,7 @@ case $choice in
         run_cmd "systemctl status xray --no-pager"
         ;;
     16)
-        run_cmd "find /var/log -type f -regex '.*\.[01]' -print -delete"
+        run_cmd "find /var/log -type f -regex '.*\.\(1\|2\.gz\)' -print -delete"
         ;;
     17)
         # 增加判断，防止 setfacl 未安装导致报错
